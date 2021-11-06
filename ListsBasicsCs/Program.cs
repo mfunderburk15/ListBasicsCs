9a0431acfc1d453a706cd8a147dcd14453f15fa7
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2021-11-05 23:05:33 -0500

    first commit

---
+++ ListsBasicsCs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ListsBasicsCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
